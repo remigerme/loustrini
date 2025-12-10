c472d1dc2cd73fc1b181cdbe5d7e39dae94bbc98
remi.germe <remi.germe@gmail.com>
2025-12-10 09:52:00 +0100

    func_calls simpler: using substitute

--- bin/func_calls.ml
+++ bin/func_calls.ml
@@ -13,6 +13,7 @@ let ctx = mk_context []
 let int_s = Arithmetic.Integer.mk_sort ctx
 let _bool_s = Boolean.mk_sort ctx
 let one = Arithmetic.Integer.mk_numeral_i ctx 1
+let n_global = declare_const ctx "n" int_s
 
 (* Storing FuncDecl *)
 let func_decls = Hashtbl.create 10
@@ -25,7 +26,7 @@ let register_func_decl name in_s out_s =
 let func_defs = ref []
 
 (* Defining incr *)
-let def_incr _x call =
+let def_incr (x : Expr.expr) (call : int) =
   (* Naming symbols *)
   let xn = "x_" ^ string_of_int call in
   let ln = "l_" ^ string_of_int call in
@@ -36,56 +37,57 @@ let def_incr _x call =
   register_func_decl ln [ int_s ] int_s;
   register_func_decl yn [ int_s ] int_s;
 
-  let def_l n =
+  let def_l =
     Boolean.mk_eq ctx
-      (Expr.mk_app ctx (Hashtbl.find func_decls ln) [ n ])
+      (Expr.mk_app ctx (Hashtbl.find func_decls ln) [ n_global ])
       (Arithmetic.mk_add ctx
-         [ Expr.mk_app ctx (Hashtbl.find func_decls xn) [ n ]; one ])
+         [ Expr.mk_app ctx (Hashtbl.find func_decls xn) [ n_global ]; one ])
   in
 
-  let def_y n =
+  let def_y =
     Boolean.mk_eq ctx
-      (Expr.mk_app ctx (Hashtbl.find func_decls yn) [ n ])
+      (Expr.mk_app ctx (Hashtbl.find func_decls yn) [ n_global ])
       (Arithmetic.mk_add ctx
          [
-           Expr.mk_app ctx (Hashtbl.find func_decls xn) [ n ];
-           Expr.mk_app ctx (Hashtbl.find func_decls ln) [ n ];
+           Expr.mk_app ctx (Hashtbl.find func_decls xn) [ n_global ];
+           Expr.mk_app ctx (Hashtbl.find func_decls ln) [ n_global ];
          ])
   in
 
-  (* We'd like to plumb inputs _x here too, but we can't as we need a function of n, 
-     and at this point _x is already evaluated for a given n. *)
-  func_defs := def_l :: def_y :: !func_defs
+  let def_x =
+    Boolean.mk_eq ctx
+      (Expr.mk_app ctx (Hashtbl.find func_decls xn) [ n_global ])
+      x
+  in
+
+  func_defs := def_l :: def_y :: def_x :: !func_defs;
+
+  Expr.mk_app ctx (Hashtbl.find func_decls yn) [ n_global ]
 
 let a = declare_func ctx "a" [ int_s ] int_s
 let b = declare_func ctx "b" [ int_s ] int_s
 let z = declare_func ctx "z" [ int_s ] int_s
 let t = declare_func ctx "t" [ int_s ] int_s
 
-let def_z n =
-  (* Here providing a [ n ] would prevent correctly plumbing inputs. *)
-  def_incr () 0;
-  Boolean.mk_eq ctx (Expr.mk_app ctx z [ n ])
-    (Expr.mk_app ctx (Hashtbl.find func_decls "y_0") [ n ])
-
-let def_t n =
-  (* Here providing b [ n ] would prevent correctly plumbing inputs. *)
-  def_incr () 1;
-  Boolean.mk_eq ctx (Expr.mk_app ctx t [ n ])
-    (Expr.mk_app ctx (Hashtbl.find func_decls "y_1") [ n ])
-
-let def_x0 n =
+let def_z =
   Boolean.mk_eq ctx
-    (Expr.mk_app ctx (Hashtbl.find func_decls "x_0") [ n ])
-    (Expr.mk_app ctx a [ n ])
+    (Expr.mk_app ctx z [ n_global ])
+    (def_incr (Expr.mk_app ctx a [ n_global ]) 0)
 
-let def_x1 n =
+let def_t =
   Boolean.mk_eq ctx
-    (Expr.mk_app ctx (Hashtbl.find func_decls "x_1") [ n ])
-    (Expr.mk_app ctx b [ n ])
+    (Expr.mk_app ctx t [ n_global ])
+    (def_incr (Expr.mk_app ctx b [ n_global ]) 1)
 
-let () = func_defs := def_z :: def_t :: def_x0 :: def_x1 :: !func_defs
-let incr_delta n = Boolean.mk_and ctx (List.map (fun x -> x n) !func_defs)
+let () = func_defs := def_z :: def_t :: !func_defs
+
+let incr_delta n =
+  let aux expr = Expr.substitute_one expr n_global n in
+  let exprs = List.map aux !func_defs in
+  Boolean.mk_and ctx exprs
+
 let n = declare_const ctx "n" int_s
 let d = incr_delta n
+let dp = incr_delta (Arithmetic.mk_add ctx [ n; one ])
 let () = print_endline (Expr.to_string d)
+let () = print_endline (Expr.to_string dp)
