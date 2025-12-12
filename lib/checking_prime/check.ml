e874ab25aa63b1d6c15d264e6f7bc546c7780543
remi.germe <remi.germe@gmail.com>
2025-12-12 13:48:14 +0100

    skeleton of prime encoding

---
+++ lib/checking_prime/check.ml
@@ -0,0 +1,40 @@
+open Z3
+open Env
+
+type error = NonBooleanProperty
+
+exception Error of error
+
+(** Helper as [Boolean.mk_implies] do not support lists of expressions. *)
+let mk_implies ctx (p : Expr.expr list) (q : Expr.expr list) =
+  let nq = List.map (fun e -> Boolean.mk_not ctx e) q in
+  Boolean.mk_and ctx (p @ nq)
+
+let prove ctx env prop =
+  (* We check that the given property is a boolean. *)
+  let () =
+    match Sort.get_sort_kind (Expr.get_sort prop) with
+    | BOOL_SORT -> ()
+    | _ -> raise (Error NonBooleanProperty)
+  in
+
+  (* Creating a solver *)
+  let solver = Solver.mk_solver ctx None in
+
+  (* Base case *)
+  let query0 = mk_implies ctx (get_eqs ctx env @ init ctx env) [ prop ] in
+  (match Solver.check solver [ query0 ] with
+  | SATISFIABLE -> print_endline "sat"
+  | UNSATISFIABLE -> print_endline "unsat"
+  | UNKNOWN -> print_endline "unknown");
+
+  (* Consecution *)
+  let eqs = get_eqs ctx env in
+  let eqs_prime = get_eqs_prime ctx env in
+  let trans_eqs = trans ctx env in
+  let prop_prime = prime_expr ctx env prop in
+  let query_ind = mk_implies ctx (eqs @ eqs_prime @ trans_eqs) [ prop_prime ] in
+  match Solver.check solver [ query_ind ] with
+  | SATISFIABLE -> print_endline "sat"
+  | UNSATISFIABLE -> print_endline "unsat"
+  | UNKNOWN -> print_endline "unknown"
---
+++ lib/checking_prime/env.ml
@@ -0,0 +1,66 @@
+open Z3
+
+type var_t = { name : string; sort : Sort.sort; def : Expr.expr }
+
+type state_var_t = {
+  name : string;
+  sort : Sort.sort;
+  (* Might be none if the init value is not specified (e.g. state storing a `pre`). *)
+  init : Expr.expr option;
+  next : Expr.expr;
+}
+
+type z3_env_t = { vars : var_t list; state_vars : state_var_t list }
+
+let name_prime name = name ^ "_p"
+let expr_of_var ctx (v : var_t) = Expr.mk_const_s ctx v.name v.sort
+let expr_of_state_var ctx (s : state_var_t) = Expr.mk_const_s ctx s.name s.sort
+
+let expr_of_var_prime ctx (v : var_t) =
+  Expr.mk_const_s ctx (name_prime v.name) v.sort
+
+let expr_of_state_var_prime ctx (s : state_var_t) =
+  Expr.mk_const_s ctx (name_prime s.name) s.sort
+
+(** Returns the expression where every variable has been replaced by its primed version. *)
+let prime_expr ctx env (e : Expr.expr) =
+  (* Fetching all variables *)
+  let vars = List.map (expr_of_var ctx) env.vars in
+  let state_vars = List.map (expr_of_state_var ctx) env.state_vars in
+  let all_vars = vars @ state_vars in
+
+  (* Fetching all primed variables *)
+  let vars_prime = List.map (expr_of_var_prime ctx) env.vars in
+  let state_vars_prime =
+    List.map (expr_of_state_var_prime ctx) env.state_vars
+  in
+  let all_vars_prime = vars_prime @ state_vars_prime in
+
+  (* Replacing every variable by its primed version *)
+  Expr.substitute e all_vars all_vars_prime
+
+let get_eqs ctx env =
+  let eq_var v = Boolean.mk_eq ctx (expr_of_var ctx v) v.def in
+  let eqs_var = List.map eq_var env.vars in
+  eqs_var
+
+let get_eqs_prime ctx env =
+  let eqs = get_eqs ctx env in
+  List.map (prime_expr ctx env) eqs
+
+(** Returns equations satisfied by [state_vars] at initiation. *)
+let init ctx env =
+  let def_init (s : state_var_t) =
+    match s.init with
+    | None -> None
+    | Some e -> Some (Boolean.mk_eq ctx (expr_of_state_var ctx s) e)
+  in
+  List.filter_map def_init env.state_vars
+
+(** Returns equations satisfied by primed [state_vars] when doing one step. *)
+let trans ctx env =
+  let def_next (s : state_var_t) =
+    let s_prime = expr_of_state_var_prime ctx s in
+    Boolean.mk_eq ctx s_prime s.next
+  in
+  List.map def_next env.state_vars
