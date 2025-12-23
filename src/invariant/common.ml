6295163d3430a9f3a33a97ba98c83bbf0701b975
remi.germe <remi.germe@gmail.com>
2025-12-23 23:37:50 +0100

    maybe empty ands

--- src/invariant/common.ml
+++ src/invariant/common.ml
@@ -1,6 +1,10 @@
 open Z3
 
+let maybe_empty_and ctx (e : Expr.expr list) =
+  match e with [] -> Boolean.mk_true ctx | e -> Boolean.mk_and ctx e
+
 (** Helper as [Boolean.mk_implies] do not support lists of expressions. *)
 let mk_implies ctx (p : Expr.expr list) (q : Expr.expr list) =
-  let nq = List.map (fun e -> Boolean.mk_not ctx e) q in
-  Boolean.mk_and ctx (p @ nq)
+  let p_and = maybe_empty_and ctx p in
+  let q_and = maybe_empty_and ctx q in
+  Boolean.mk_and ctx [ p_and; Boolean.mk_not ctx q_and ]
