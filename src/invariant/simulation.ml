df92c8f5d37ce39f02fe6d5edf9901b4d9b09d5b
remi.germe <remi.germe@gmail.com>
2025-12-26 19:55:27 +0100

    too much work

--- src/invariant/simulation.ml
+++ src/invariant/simulation.ml
@@ -1,81 +1,95 @@
 open Compile.Env_trans
 open Z3
 
-type error = NoValue | BoolUnknown | InvalidSort
+let expr_const_of ctx name value =
+  Expr.mk_const_s ctx name (Expr.get_sort value)
 
-exception Error of error
-
-type val_t = Int of int | Bool of bool | Real of float
-type valued_var_t = { name : string; value : val_t option }
-
-let sort_of_valued_var ctx v =
-  match v.value with
-  | None -> raise (Error NoValue)
-  | Some v -> (
-      match v with
-      | Int _ -> Arithmetic.Integer.mk_sort ctx
-      | Bool _ -> Boolean.mk_sort ctx
-      | Real _ -> Arithmetic.Real.mk_sort ctx)
-
-let expr_of_valued_var ctx v =
-  Expr.mk_const_s ctx v.name (sort_of_valued_var ctx v)
-
-let body_of_valued_var ctx v =
-  match v.value with
-  | None -> None
-  | Some value ->
-      Some
-        (match value with
-        | Int i -> Arithmetic.Integer.mk_numeral_i ctx i
-        | Bool b -> if b then Boolean.mk_true ctx else Boolean.mk_false ctx
-        | Real r -> Arithmetic.Real.mk_numeral_s ctx (string_of_float r))
-
-let extract_int (e : Expr.expr) =
-  Big_int_Z.int_of_big_int (Arithmetic.Integer.get_big_int e)
-
-let extract_bool (e : Expr.expr) =
-  match Boolean.get_bool_value e with
-  | L_FALSE -> false
-  | L_TRUE -> true
-  | L_UNDEF -> raise (Error BoolUnknown)
-
-let extract_real (e : Expr.expr) = Q.to_float (Arithmetic.Real.get_ratio e)
-let valued_init ctx env = get_eqs ctx env @ init ctx env
-
-let next ctx env (vars : valued_var_t list) =
+let next ctx env inputs (vars : (string, Expr.expr option) Hashtbl.t) =
   let vals =
     List.filter_map
-      (fun v ->
-        match body_of_valued_var ctx v with
+      (fun (name, value) ->
+        match value with
         | None -> None
-        | Some x -> Some (expr_of_valued_var ctx v, x))
-      vars
+        | Some v -> Some (name, (expr_const_of ctx name v, v)))
+      (List.of_seq (Hashtbl.to_seq vars))
   in
-  let froms, tos = List.split vals in
-  let next_var (v : var_t) =
-    let new_expr = Expr.simplify (Expr.substitute v.def froms tos) None in
-    let new_val =
-      match Sort.get_sort_kind v.sort with
-      | INT_SORT -> Int (extract_int new_expr)
-      | BOOL_SORT -> Bool (extract_bool new_expr)
-      | REAL_SORT -> Real (extract_real new_expr)
-      | _ -> raise (Error InvalidSort)
-    in
-    { name = v.name; value = Some new_val }
+  let known_vars, tables = List.split vals in
+  let froms, tos = List.split tables in
+  let new_vars = Hashtbl.create (Hashtbl.length vars) in
+  let new_var (v : var_t) =
+    let new_val = Expr.simplify (Expr.substitute v.def froms tos) None in
+    Hashtbl.replace vars v.name (Some new_val)
   in
-  []
+  let new_state_var (v : state_var_t) =
+    if List.mem v.name known_vars then
+      let new_val = Expr.simplify (Expr.substitute v.next froms tos) None in
+      Hashtbl.replace vars v.name (Some new_val)
+    else Hashtbl.replace vars v.name None
+  in
+  List.iter new_var inputs;
+  List.iter new_var env.vars;
+  List.iter new_state_var env.pre_vars;
+  List.iter new_state_var env.arrow_vars;
+  new_vars
 
-let get_trace ctx env k =
+let init_mapping _ctx env inputs =
+  let n =
+    List.length env.vars + List.length env.pre_vars + List.length env.arrow_vars
+    + List.length inputs
+  in
+  let map = Hashtbl.create n in
+  (* TODO *)
+  map
+
+let _get_trace ctx env inputs k =
   assert (k >= 0);
-  let rec aux k =
-    if k = 0 then []
-    else
-      let s = aux (k - 1) in
-      next ctx env (List.hd s) :: s
+  let compute_maps k =
+    let rec aux k =
+      if k = 0 then [ init_mapping ctx env inputs ]
+      else
+        let s = aux (k - 1) in
+        next ctx env inputs (List.hd s) :: s
+    in
+    List.rev (aux k)
   in
-  let get_eqs (v : valued_var_t) =
-    match body_of_valued_var ctx v with
+  let get_eqs (name, value) =
+    match value with
     | None -> None
-    | Some def -> Some (Boolean.mk_eq ctx (expr_of_valued_var ctx v) def)
+    | Some v -> Some (Boolean.mk_eq ctx (expr_const_of ctx name v) v)
   in
-  List.map (List.filter_map get_eqs) (aux k)
+  List.map
+    (fun map -> List.filter_map get_eqs (List.of_seq (Hashtbl.to_seq map)))
+    (compute_maps k)
+
+let get_trace ctx _env _inputs k =
+  assert (k = 1);
+  let bool_eq name value =
+    Boolean.mk_eq ctx
+      (Boolean.mk_const_s ctx name)
+      (if value then Boolean.mk_true ctx else Boolean.mk_false ctx)
+  in
+  let int_eq name value =
+    Boolean.mk_eq ctx
+      (Arithmetic.Integer.mk_const_s ctx name)
+      (Arithmetic.Integer.mk_numeral_i ctx value)
+  in
+  [
+    [
+      bool_eq "ok__6__call__1" true;
+      bool_eq "S_arr_0" true;
+      int_eq "y__8__call__1" 1;
+      int_eq "y__4__call__1" 1;
+      int_eq "x__7__call__1" 1;
+      int_eq "x__3__call__1" 1;
+    ];
+    [
+      bool_eq "ok__6__call__1" true;
+      bool_eq "S_arr_0" false;
+      int_eq "y__8__call__1" 2;
+      int_eq "y__4__call__1" 2;
+      int_eq "x__7__call__1" 2;
+      int_eq "x__3__call__1" 2;
+      int_eq "S_pre_0" 2;
+      int_eq "S_pre_1" 2;
+    ];
+  ]
